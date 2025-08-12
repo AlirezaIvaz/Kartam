21e9dff7c956a0c4df755b4e43db35cc76b8be04
Alireza Ivaz <ivaz1382@gmail.com>
2025-08-12 14:53:42 +0330

    Add negative offset to toolbar menu

--- app/src/main/kotlin/ir/alirezaivaz/kartam/ui/screens/HomeScreen.kt
+++ app/src/main/kotlin/ir/alirezaivaz/kartam/ui/screens/HomeScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.lifecycle.Lifecycle
@@ -137,6 +138,10 @@ fun HomeScreen() {
                             }
                             DropdownMenu(
                                 expanded = showOptionsMenu,
+                                offset = DpOffset(
+                                    x = (-8).dp,
+                                    y = 0.dp
+                                ),
                                 onDismissRequest = {
                                     showOptionsMenu = false
                                 }
