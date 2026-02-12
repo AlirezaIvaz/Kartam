39d407b007372a6f91d47cfc7a34a6ee89dd844b
Alireza Ivaz <ivaz1382@gmail.com>
2026-02-12 16:21:46 +0330

    Fixed crash in auto-restore (closes #12)

--- app/src/main/kotlin/ir/alirezaivaz/kartam/dto/Backup.kt
+++ app/src/main/kotlin/ir/alirezaivaz/kartam/dto/Backup.kt
@@ -1,7 +1,12 @@
 package ir.alirezaivaz.kartam.dto
 
+import com.google.gson.annotations.SerializedName
+
 data class Backup(
+    @SerializedName("version")
     val version: Int,
+    @SerializedName("timestamp")
     val timestamp: Long,
+    @SerializedName("cards")
     val cards: List<CardInfo>
 )
