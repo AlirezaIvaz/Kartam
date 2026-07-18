01bcce834f77d48a375e6d4e6908011987c4039d
Alireza Ivaz <ivaz1382@gmail.com>
2026-07-18 13:56:11 +0330

    Add serialized name to prevent proguard problems

--- app/src/main/kotlin/ir/alirezaivaz/kartam/dto/AccountType.kt
+++ app/src/main/kotlin/ir/alirezaivaz/kartam/dto/AccountType.kt
@@ -1,15 +1,21 @@
 package ir.alirezaivaz.kartam.dto
 
 import androidx.annotation.StringRes
+import com.google.gson.annotations.SerializedName
 import ir.alirezaivaz.kartam.R
 
 enum class AccountType(
     @param:StringRes
     val title: Int,
 ) {
+    @SerializedName("ActiveQarzAlHasaneh")
     ActiveQarzAlHasaneh(title = R.string.account_active_qarz_al_hasaneh),
+    @SerializedName("SavingQarzAlHasaneh")
     SavingQarzAlHasaneh(title = R.string.account_saving_qarz_al_hasaneh),
+    @SerializedName("ShortTermDeposit")
     ShortTermDeposit(title = R.string.account_short_term_deposit),
+    @SerializedName("LongTermDeposit")
     LongTermDeposit(title = R.string.account_long_term_deposit),
+    @SerializedName("JointAccount")
     JointAccount(title = R.string.account_joint);
 }
